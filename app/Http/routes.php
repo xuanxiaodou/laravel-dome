5ebfbce58794bc1b48eb0d1e13a03c95d44e2f5c
xuanxiaodou <2104727316@qq.com>
2017-03-30 00:15:45 +0800

    11111

---
+++ app/Http/routes.php
@@ -0,0 +1,37 @@
+<?php
+use Illuminate\Support\Facades\Redis;
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    //Redis::set('lar','laravel!!!');
+   // return Redis::get('lar');
+    	Redis::set('string:user:name','yao');
+	$pwd = Redis::get('string:user:name');
+	return $pwd;
+	return view('welcome');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
---
+++ info.php
@@ -0,0 +1,9 @@
+<?php
+	$conn=mysql_connect('127.0.0.1','root','root');
+if ($conn){
+  echo "LNMP platform connect to mysql is successful!";
+}else{
+  echo "LNMP platform connect to mysql is failed!";
+}
+ phpinfo();
+?>
